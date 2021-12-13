5bae11c88de50002f1c8a10403fcf6392f964242
joeys1122 <josephs1122@gmail.com>
2021-12-13 14:09:38 -0500

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -30,7 +32,12 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Route path='/'>
+        <MovieList movies={movieList} />
+      </Route>
+      <Route path='/movies/:id'>
+        <Movie />
+      </Route>
     </div>
   );
 }
